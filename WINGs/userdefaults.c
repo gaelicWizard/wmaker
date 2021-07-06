674ecf68d2e2886be0ef12dee02785490cc27301
John D Pell <John+git@gaelicWizard.net>
2021-07-05 18:13:18 -0700

    WINGs: use GNUSTEP_USER_DEFAULTS_DIR in userdefaults.c
    If available, use environment variable GNUSTEP_USER_DEFAULTS_DIR for path to user's GNUstep defaults directory. Otherwise, fall back to current "~/GNUstep/Defaults". This alsö honors WMAKER_USER_ROOT.
    
    The previous code seems to have mostly worked as both the old ('gnustep') and new ('fhs') filesystem layouts for GNUstep specify the home-relative user defaults path as 'GNUstep/Defaults', but would never have worked with 'Apple', 'mac', 'next', or 'standalone'. The previous code alsö required the user to export GNUSTEP_USER_ROOT (recently changed to WMAKER_USER_ROOT), which was deprecated in GNUstep v2 (2007/04/13).
    
    Alsö, modify wusergnusteppath() and wdefaultspathfordomain(char*) to use wstrconcat(char*,char*) and wstrappend(char*,char*) instead of duplicating the string code. Always use 'wstrappend(wstrconcat(char*,char*),char*)' so that pointers from getenv(char*) are always copied before realloc(). (This is ripe for a new helper function 'wpathappend(char*,char*)' to add the path separator without this weird constuct.)

--- WINGs/userdefaults.c
+++ WINGs/userdefaults.c
@@ -13,6 +13,12 @@
 #include "WINGsP.h"
 #include "userdefaults.h"
 
+#ifdef HAVE_SECURE_GETENV
+#define GETENV(x) secure_getenv((x))
+#else
+#define GETENV(x) getenv((x))
+#endif
+
 
 typedef struct W_UserDefaults {
 	WMPropList *defaults;
@@ -40,7 +46,11 @@ char *WMUserDefaultsDidChangeNotification = "WMUserDefaultsDidChangeNotification
 
 static void synchronizeUserDefaults(void *foo);
 
-#define DEFAULTS_DIR "/Defaults"
+#ifndef DEFAULTS_DIR
+#define GSDEFAULTS_DIR "GNUstep/Defaults"
+#define DEFAULTS_DIR "Defaults"
+#define GSUSER_DIR "GNUstep"
+#endif
 #ifndef HAVE_INOTIFY
 /* Check defaults database for changes every this many milliseconds */
 /* XXX: this is shared with src/ stuff, put it in some common header */
@@ -49,20 +59,14 @@ static void synchronizeUserDefaults(void *foo);
 
 const char *wusergnusteppath()
 {
-	static const char subdir[] = "/GNUstep";
 	static char *path = NULL;
-	char *gspath, *h;
-	int pathlen;
+	char *gspath, *homedir;
 
 	if (path)
 		/* Value have been already computed, re-use it */
 		return path;
 
-#ifdef HAVE_SECURE_GETENV
-	gspath = secure_getenv("WMAKER_USER_ROOT");
-#else
-	gspath = getenv("WMAKER_USER_ROOT");
-#endif
+	gspath = GETENV("WMAKER_USER_ROOT");
 	if (gspath) {
 		gspath = wexpandpath(gspath);
 		if (gspath) {
@@ -72,14 +76,11 @@ const char *wusergnusteppath()
 		wwarning(_("variable WMAKER_USER_ROOT defined with invalid path, not used"));
 	}
 
-	h = wgethomedir();
-	if (!h)
+	homedir = wgethomedir();
+	if (!homedir)
 		return NULL;
 
-	pathlen = strlen(h);
-	path = wmalloc(pathlen + sizeof(subdir));
-	strcpy(path, h);
-	strcpy(path + pathlen, subdir);
+	path = wstrappend(wstrconcat(homedir, "/"), GSUSER_DIR);
 
 	return path;
 }
@@ -87,17 +88,26 @@ const char *wusergnusteppath()
 char *wdefaultspathfordomain(const char *domain)
 {
 	char *path;
-	const char *gspath;
-	size_t slen;
+	const char *homedir, *udefpath;
 
-	gspath = wusergnusteppath();
-	slen = strlen(gspath) + strlen(DEFAULTS_DIR) + strlen(domain) + 4;
-	path = wmalloc(slen);
+	homedir = wgethomedir();
+	if (!homedir)
+		return NULL;
 
-	strcpy(path, gspath);
-	strcat(path, DEFAULTS_DIR);
-	strcat(path, "/");
-	strcat(path, domain);
+	udefpath = GETENV("GNUSTEP_USER_DEFAULTS_DIR");
+	if (udefpath) {
+		udefpath = wexpandpath(wstrappend(wstrconcat(homedir, "/"), udefpath));
+
+		if (!udefpath) {
+			wwarning(_("variable GNUSTEP_USER_DEFAULTS_DIR defined with invalid path, not used"));
+		}
+	} else if (GETENV("WMAKER_USER_ROOT")) {
+		udefpath = wexpandpath(wstrappend(wstrconcat(wusergnusteppath(), "/"), DEFAULTS_DIR));
+	} else {
+		udefpath = wexpandpath(wstrappend(wstrconcat(homedir, "/"), GSDEFAULTS_DIR));
+	}
+
+	path = wstrappend(wstrconcat(udefpath, "/"), domain);
 
 	return path;
 }
